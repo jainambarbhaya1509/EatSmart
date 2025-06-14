8675228033da411932af3570eb2a533984242bf6
jainambarbhaya1509 <jainambarbhaya1509@gmail.com>
2025-06-15 01:55:56 +0530

    anywhere llm chatbot

---
+++ anythingllm/health_chatbot/chatbot.py
@@ -0,0 +1,52 @@
+import requests
+import yaml
+from flask import Flask, request, jsonify
+
+class Chatbot:
+    def __init__(self):
+        with open("config.yaml", "r") as file:
+            config = yaml.safe_load(file)
+
+        self.api_key = config["api_key"]
+        self.base_url = config["model_server_base_url"]
+        self.workspace_slug = config["workspace_slug"]
+
+        self.chat_url = f"{self.base_url}/workspace/{self.workspace_slug}/chat"
+
+        self.headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self.api_key
+        }
+
+    def chat(self, message: str) -> str:
+        data = {
+            "message": message,
+            "mode": "chat",
+            "sessionId": "example-session-id",
+            "attachments": []
+        }
+        chat_response = requests.post(
+            self.chat_url,
+            headers=self.headers,
+            json=data
+        )
+        try:
+            return chat_response.json()['textResponse']
+        except ValueError:
+            return "Response is not valid JSON"
+        except Exception as e:
+            return f"Chat request failed. Error: {e}"
+
+app = Flask(__name__)
+chatbot = Chatbot()
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.get_json()
+    message = data.get("message", "")
+    response = chatbot.chat(message)
+    return jsonify({"response": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- eatsmart_edge/src/schema/height_estimation_schema.js
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { zodToJsonSchema } from 'zod-to-json-schema';
-
-
-export const HeightEstSchema = z.object({
-  height: z.string()
-});
-
-export const heightSchema = zodToJsonSchema(HeightEstSchema, "HeightEstSchema");
